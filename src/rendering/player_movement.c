1bca29ad793db0a449dbd41ec8f591b14d9b27ab
coincoin94360 <christianmarques94360@gmail.com>
2026-02-17 09:20:38 +0100

    "Add sliding movement.Modify render.c and player_movement.c"

--- src/rendering/player_movement.c
+++ src/rendering/player_movement.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player_movement.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsenniko <vsenniko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chmarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 06:01:16 by chmarque          #+#    #+#             */
-/*   Updated: 2025/12/27 17:09:56 by vsenniko         ###   ########.fr       */
+/*   Updated: 2026/02/17 09:18:25 by chmarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ static void	move_backward(t_player *player, float cos_a, float sin_a,
 
 	new_x = player->x_player - (cos_a * player->speed);
 	new_y = player->y_player - (sin_a * player->speed);
-	if (!check_collision(new_x, new_y, game))
+	/* if (!check_collision(new_x, new_y, game))
 	{
 		player->x_player = new_x;
 		player->y_player = new_y;
-	}
+	} */
+	if (!check_collision(new_x, player->y_player, game))
+		player->x_player = new_x;
+	if (!check_collision(player->x_player, new_y, game))
+		player->y_player = new_y;
 }
 
 static void	move_left(t_player *player, float cos_a, float sin_a, t_game *game)
@@ -34,11 +38,15 @@ static void	move_left(t_player *player, float cos_a, float sin_a, t_game *game)
 
 	new_x = player->x_player + (sin_a * player->speed);
 	new_y = player->y_player - (cos_a * player->speed);
-	if (!check_collision(new_x, new_y, game))
+	/* if (!check_collision(new_x, new_y, game))
 	{
 		player->x_player = new_x;
 		player->y_player = new_y;
-	}
+	} */
+	if (!check_collision(new_x, player->y_player, game))
+		player->x_player = new_x;
+	if (!check_collision(player->x_player, new_y, game))
+		player->y_player = new_y;
 }
 
 static void	move_right(t_player *player, float cos_a, float sin_a, t_game *game)
@@ -48,11 +56,15 @@ static void	move_right(t_player *player, float cos_a, float sin_a, t_game *game)
 
 	new_x = player->x_player - (sin_a * player->speed);
 	new_y = player->y_player + (cos_a * player->speed);
-	if (!check_collision(new_x, new_y, game))
+	/* if (!check_collision(new_x, new_y, game))
 	{
 		player->x_player = new_x;
 		player->y_player = new_y;
-	}
+	} */
+	if (!check_collision(new_x, player->y_player, game))
+		player->x_player = new_x;
+	if (!check_collision(player->x_player, new_y, game))
+		player->y_player = new_y;
 }
 
 void	apply_movement(t_player *player, float cos_a, float sin_a, t_game *game)
--- src/rendering/render.c
+++ src/rendering/render.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chrmarqu <chrmarqu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chmarque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 10:00:00 by chmarque          #+#    #+#             */
-/*   Updated: 2026/01/22 18:52:34 by chrmarqu         ###   ########.fr       */
+/*   Updated: 2026/02/17 09:15:54 by chmarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,15 @@ void	move_player(t_game *game)
 	{
 		new_x = game->player.x_player + (cos_angle * game->player.speed);
 		new_y = game->player.y_player + (sin_angle * game->player.speed);
-		if (!check_collision(new_x, new_y, game))
+		/* if (!check_collision(new_x, new_y, game))
 		{
 			game->player.x_player = new_x;
 			game->player.y_player = new_y;
-		}
+		} */
+		if (!check_collision(new_x, game->player.y_player, game))
+			game->player.x_player = new_x;
+		if (!check_collision(game->player.x_player, new_y, game))
+			game->player.y_player = new_y;
 	}
 	apply_movement(&game->player, cos_angle, sin_angle, game);
 }
